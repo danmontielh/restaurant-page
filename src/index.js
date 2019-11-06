9cf4d6ea013f8f2e603b7ad234fc0536659b646e
Dan Montiel <danmontielh@gmail.com>
2019-11-05 23:14:06 -0600

    enable menuListener and create component contact. It's works

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
 import header from './components/header.js';
 import homePage from './components/homepage.js';
+import contact from './components/contact.js'
 import { createDivWithId, renderComponent, menuListener } from "./components/helpers.js";
 
 // SET Header and divs
@@ -13,4 +14,19 @@ createDivWithId('content', 'app');
 const app = document.getElementById('content');
 const menu = Array.from(document.getElementsByTagName('a'));
 
+
+//Load Homepage Component
 renderComponent(app, homePage);
+
+
+// Navegation Handler Function
+
+const handleNavegation = ($event) => {
+    if ($event.target.innerText == 'Contact') {
+        renderComponent(app, contact);
+    } else {
+        renderComponent(app, homePage);
+    }
+};
+
+menuListener(menu, handleNavegation);
